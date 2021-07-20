c769c8de7430403aded4bafa4e773b170209f314
Selorm Adonu <selorm958@gmail.com>
2021-07-19 20:01:23 -0400

    different editions

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def prim():
-   print("Keep it logically awesome with Love.")
+   #print("Keep it logically awesome with Love.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
   prim()
